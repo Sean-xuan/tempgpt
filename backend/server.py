7aab685066d10fb446d4bb8231db3a90fe1a1265
zxsean <xuzixuan0216@outlook.com>
2023-09-09 19:57:59 +0900

    Remove whisper import

--- backend/server.py
+++ backend/server.py
@@ -163,4 +163,5 @@ def EL_TTS(message,response_audio_filename):
     return audio_content.duration_seconds
 
 if __name__ == '__main__':
-    app.run(port=5002)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
